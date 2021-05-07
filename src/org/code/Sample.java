ad0026296240230857cbe909a405810c8ea1362d
gopalkrish0015 <gopalkrish0015@gmail.com>
2021-05-07 19:12:32 +0530

    check the code

---
+++ src/org/code/Sample.java
@@ -0,0 +1,14 @@
+package org.code;
+
+public class Sample {
+public static void main(String[] args) {
+	
+	String name = "one plus";
+	System.out.println(name);
+	int a= 10;
+	System.out.println(a);
+	
+	
+}
+
+}
