d159852838c036b238b146b7e546a4cb122a97bf
gopalkrish0015 <gopalkrish0015@gmail.com>
2021-05-07 20:01:02 +0530

    check the code in b

--- src/org/code/Sample.java
+++ src/org/code/Sample.java
@@ -9,6 +9,15 @@ public static void main(String[] args) {
 	System.out.println(a);
 	int b=20;
 	System.out.println(b);
+	int c=30;
+	System.out.println(c);
+	int g=50;
+	System.out.println(70);
+	
+	
+	
+	
+	
 	
 	
 }
